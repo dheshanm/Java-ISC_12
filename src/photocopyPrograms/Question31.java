439b698069a4278646e6c2792cc847c94867eaac
D-codex <dheshan.mail@gmail.com>
2016-12-19 20:19:31 +0530

    Added Question31

---
+++ src/photocopyPrograms/Question31.java
@@ -0,0 +1,37 @@
+package photocopyPrograms;
+import java.util.*;
+
+/**
+ * Created by D'codex on December 2016.
+ */
+public class Question31 {
+    int N;
+    Question31(int n){
+        int N=n;
+    }
+    Question31(){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the Number N  :");
+        int n = in.nextInt();
+        N=n;
+    }
+    void initiate() {
+        partition(N, N, "");
+    }
+    void partition(int n, int max, String prefix) {
+        if (n == 0) {
+            System.out.println(prefix);
+            return;
+        }
+
+        for (int i = Math.min(max, n); i >= 1; i--) {
+            partition(n-i, i, prefix + " " + i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Question31 ob=new Question31();
+        ob.initiate();
+    }
+}
