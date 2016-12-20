ae5a427650a957f1b2e556d0f629a51b30617257
D-codex <dheshan.mail@gmail.com>
2016-12-20 21:24:28 +0530

    Edit from Home Optimised Mobius Function class

--- src/photocopyPrograms/Mobius.java
+++ src/photocopyPrograms/Mobius.java
@@ -5,12 +5,29 @@ import java.util.*;
  * Created by D'codex on December 2016.
  */
 public class Mobius {
+    static int count;
     static boolean isPrime(int input){
         boolean flag=true;
         for(int i=2;i<input;i++){
             if(input%i==0)
                 flag=false;
         }
+        return flag;
+    }
+    public static boolean isRepeating(String data) {
+        HashMap<String, Integer> repeated = new LinkedHashMap<String, Integer>();
+        boolean flag=false;
+        for(String word:data.split(" ")){
+            Integer count=repeated.get(word);
+            if(count==null)
+                count=0;
+            else {
+                count = count + 1;
+                flag=true;
+            }
+            repeated.put(word,count);
+        }
+        count=repeated.size();
         return flag;
     }
     public static void main(String Args[]){
@@ -29,6 +46,14 @@ public class Mobius {
                     num=num/i;
                 }
             }
+        }
+        if(isRepeating(factors)) {
+            System.out.println("M(" + N + ")=0");   //Because it has repeating factors
+            System.out.println(factors);
+        }
+        else{
+            System.out.println("M(" + N + ")="+count*(-1));   //Because it has distinct factors
+            System.out.println(factors);
         }
 
     }
